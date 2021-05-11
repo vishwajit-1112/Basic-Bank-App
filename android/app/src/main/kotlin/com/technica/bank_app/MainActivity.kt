80fb980633072453de7401405c3f23d335aa4d0c
Vishwajith Sudhakar <84003547+vishwajit-1112@users.noreply.github.com>
2021-05-11 12:48:08 +0530

    Add files via upload

---
+++ android/app/src/main/kotlin/com/technica/bank_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.technica.bank_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/screens/adminDetails.dart
@@ -0,0 +1,56 @@
+import 'package:bank_app/model/customer.dart';
+import 'package:bank_app/widgets/customcard.dart';
+import 'package:flutter/material.dart';
+
+class AdminDetailsScreen extends StatefulWidget {
+  static const routeNamed = '/AdminDetails';
+
+  @override
+  _AdminDetailsScreenState createState() => _AdminDetailsScreenState();
+}
+
+class _AdminDetailsScreenState extends State<AdminDetailsScreen> {
+  @override
+  Widget build(BuildContext context) {
+    var query = ModalRoute.of(context).settings.arguments as Customer;
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.red,
+        title: Text(query.name),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 50,
+              backgroundImage: AssetImage("assets/images/customer.jpg"),
+            ),
+            Text(
+              query.name,
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.green,
+              ),
+            ),
+            const Divider(
+              color: Colors.black,
+              height: 20,
+              thickness: 2,
+              endIndent: 0,
+            ),
+            CustomCard(
+              icon: Icons.account_box_outlined,
+              title: 'Account No: ${query.id}',
+            ),
+            CustomCard(
+              icon: Icons.account_balance_wallet_sharp,
+              title: 'Balance: Rs.${query.balance}',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
